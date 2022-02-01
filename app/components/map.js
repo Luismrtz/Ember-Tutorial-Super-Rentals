e914e7c1f7b1ef16f488e675cac53870a0afc637
Luismrtz <luisricardo494@gmail.com>
2022-02-01 13:19:08 -0500

    test

--- app/components/map.js
+++ app/components/map.js
@@ -5,10 +5,12 @@ const MAPBOX_API = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static';
 
 export default class MapComponent extends Component {
   get src() {
+    // to access the passed down arguments FROM map.HBS (DATA is passed into component from within OTHER components)
     let { lng, lat, width, height, zoom } = this.args;
 
     let coordinates = `${lng},${lat},${zoom}`;
     let dimensions = `${width}x${height}`;
+    // receiving token from 'getter's token' below ?
     let accessToken = `access_token=${this.token}`;
 
     return `${MAPBOX_API}/${coordinates}/${dimensions}@2x?${accessToken}`;
